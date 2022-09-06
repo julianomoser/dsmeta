0492e94d8736462a6b273015717f69c5cd256333
Juliano Moser <juliano.moserjj@gmail.com>
2022-09-06 09:55:49 -0300

    first commit

---
+++ backend/src/test/java/com/moser/dsmeta/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moser.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
