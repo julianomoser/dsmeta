1e8dea0f8dc7d1214dc787a0b7bdef62ede6b2cf
Juliano Moser <juliano.moserjj@gmail.com>
2022-09-07 18:59:59 -0300

    Database

---
+++ backend/src/main/java/com/moser/dsmeta/entities/Selle.java
@@ -0,0 +1,46 @@
+package com.moser.dsmeta.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * @author Juliano Moser
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_sales")
+public class Selle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String sellerName;
+    private Integer visited;
+    private Integer deals;
+    private BigDecimal amount;
+    private LocalDate date;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Selle selle = (Selle) o;
+        return Objects.equals(id, selle.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
